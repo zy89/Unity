0ac920899c86a8c05e7f4cd8764e81c1ebe9e14c
zy89 <yzhiyuan0321@gmail.com>
2019-09-24 11:42:17 +1000

    9控制对小球方向的影响

--- PongGameProject/Assets/Scripts/Ball.cs
+++ PongGameProject/Assets/Scripts/Ball.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     private Rigidbody2D rigidbody2D;//施加力需要得到刚体
+    
+    
 
     // Start is called before the first frame update
     void Start()
@@ -18,9 +20,18 @@ public class Ball : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    //碰撞方法
+    void OnCollisionEnter2D(Collision2D col) {
+        //通过tag判断碰撞到的是不是player
+        if(col.gameObject.tag == "Player"){
+            Vector2 velocity = rigidbody2D.velocity;//当前速度
+            //访问到player的刚体速度，直接得到y方向上的速度
+            //为了防止越撞越快，要加上碰撞速度/2
+            velocity.y = velocity.y +col.collider.GetComponent<Rigidbody2D>().velocity.y/2;
+            /*velocity.y = velocity.y/2f + col.rigidbody2D.velocity.y/2;
+            是视频的col不能直接访问到rigibody2D，过时的API
+            */
+            rigidbody2D.velocity = velocity;//赋值给小球新的速度
+        }
     }
 }
