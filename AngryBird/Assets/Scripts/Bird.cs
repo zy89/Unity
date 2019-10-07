1caa3e67183a05174daea55404761a85c06e02af
zy89 <yzhiyuan0321@gmail.com>
2019-10-07 16:33:28 +1100

    8 拖拽距离最大限定

--- AngryBird/Assets/Scripts/Bird.cs
+++ AngryBird/Assets/Scripts/Bird.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Bird : MonoBehaviour
 {
     private bool isClick = false;//默认没有点击
+    public Transform rightPos;
+    public float maxDis = 3;//默认最大距离3m
 
     private void OnMouseDown() //鼠标按下调用
     {
@@ -25,6 +27,15 @@ public class Bird : MonoBehaviour
             //给小鸟坐标：两种方法
             //transform.position += new Vector3(0,0,10);//Camera的Z是-10，因此多加个10让小鸟能被看到
             transform.position += new Vector3(0,0,-Camera.main.transform.position.z);//减去Camera的z值
+
+            //用Vector3.Distance计算两个向量的距离，结果是float的正数
+            if(Vector3.Distance(transform.position, rightPos.position) > maxDis){//进行位置限定
+            
+                //小鸟位置减去右树枝位置，得到一个向量。用normalized单位化，只要方向不要值
+                Vector3 pos = (transform.position - rightPos.position).normalized;
+                pos*= maxDis;//最大长度的向量
+                transform.position = pos + rightPos.position;//最后小鸟被限定的位置
+            }
         }
     }
 }
