4211cea4c6d2398234eb2a66bcc91bfc1baaed0a
zy89 <yzhiyuan0321@gmail.com>
2019-10-15 18:58:36 +1100

    ikun project

---
+++ Ikun Project/Assets/Files/Script/ChangeActive.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeActive : MonoBehaviour
+{
+    public GameObject obj;
+    bool status = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj.SetActive(status);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void changeActive()
+    {
+        if(status == false)
+        {
+            status = true;
+            obj.SetActive(status);
+        }
+        else
+        {
+            status = false;
+            obj.SetActive(status);
+        }
+    }
+}
---
+++ Ikun Project/Assets/Files/Script/DragObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector3 pos;
+    private float mzcoord;
+    private void OnMouseDown()
+    {
+        pos = gameObject.transform.position - getMousePos();
+        mzcoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+    }
+
+    private Vector3 getMousePos()
+    {
+        Vector3 point = Input.mousePosition;
+        pos.z = mzcoord;
+        return Camera.main.ScreenToWorldPoint(pos);
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = getMousePos() + pos;
+    }
+}
